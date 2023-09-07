319b76f323f22f34257d028de4d5e57d266589f5
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-07 15:36:34 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv) - 1
+    if (argc == 0):
+        print("{:d} arguments.".format(argc))
+    elif(argc == 1):
+        print("{:d} argument:".format(argc))
+        print("{:d}: {:s}".format(argc, argv[argc]))
+    else:
+        print("{:d} arguments:".format(argc))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
