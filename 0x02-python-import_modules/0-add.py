7710a7b920eff3848473b3cf3f12a924fbd9e8b7
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-07 14:22:16 +0000

    task 0 correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,7 +4,6 @@ from add_0 import add
 
 a = 1
 b = 2
-print("{0:d} + {1:d} = {2:d}".format(a, b, add(1, 2)))
 
 if __name__ == "__main__":
-    pass
+    print("{0:d} + {1:d} = {2:d}".format(a, b, add(1, 2)))
