2cf18a64eba32cf9e42ba5a88dedad9a6101cfad
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-08 08:50:23 +0000

    fix PEP issue

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     argc = len(argv) - 1
     if (argc == 0):
         print("{:d} arguments.".format(argc))
-    elif(argc == 1):
+    elif (argc == 1):
         print("{:d} argument:".format(argc))
         print("{:d}: {:s}".format(argc, argv[argc]))
     else:
