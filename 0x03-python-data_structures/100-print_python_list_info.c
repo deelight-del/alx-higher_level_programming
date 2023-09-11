2e48eb029b723bf02cf660a3828689c7a8028a7e
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-11 13:35:38 +0000

    add py_getitem

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -13,13 +13,15 @@
 void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t i, size, mem_alloc;
+	PyObject *element;
 
 	size = PyList_Size(p);
 	printf("[*] Size of the Python List = %zd\n", size);
-	mem_alloc = (PyListObject *)(p)->allocated;
+	mem_alloc = ((PyListObject *)(p))->allocated;
 	printf("[*] Allocated = %zd\n", mem_alloc);
 	for (i = 0; i < size; i++)
 	{
-		printf("Element %zd: %s\n", i, Py_TYPE(p)->tp_name);
+		element = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(element)->tp_name);
 	}
 }
