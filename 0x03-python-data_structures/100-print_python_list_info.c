43132be7abac36aa8cdc7c7ea197a2f339217676
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-11 19:11:07 +0000

    xxx

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <time.h>
 #include <Python.h>
+#include <listobject.h>
+#include <object.h>
 
 /**
  * print_python_list_info - Function to print information about python list
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <Python.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -22,5 +24,6 @@ void free_listint(listint_t *head);
 int *create_array(listint_t *head);
 int len_of_ll(listint_t *head);
 int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
 
 #endif /* LISTS_H */
