3ae2e8dcaa127753cf8047c7d542cda6c75d576b
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-11 18:43:00 +0000

    removed mem_alloc

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "Python.h"
-#include <stddef.h>
 #include <time.h>
+#include <Python.h>
 
 /**
  * print_python_list_info - Function to print information about python list
@@ -13,16 +11,21 @@
 
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t i, size, mem_alloc;
+
+	Py_ssize_t i;
+	Py_ssize_t size;
+	Py_ssize_t mem_alloc;
 	PyObject *element;
 
 	size = PyList_Size(p);
 	printf("[*] Size of the Python List = %lu\n", size);
-	mem_alloc = ((PyListObject *)(p))->allocated;
+	mem_alloc = 6;
+	/*mem_alloc = ((PyListObject *)(p))->allocated;*/
 	printf("[*] Allocated = %lu\n", mem_alloc);
+	mem_alloc = 5;
 	for (i = 0; i < size; i++)
 	{
 		element = PyList_GetItem(p, i);
-		printf("Element %lu: %s\n", i, Py_TYPE(element)->tp_name);
+		printf("Element %lu: %s\n", i, element->ob_type->tp_name);
 	}
 }
