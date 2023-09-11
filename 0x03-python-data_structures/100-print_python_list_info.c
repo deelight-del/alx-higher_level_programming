7864da8b52bc7e4c9e3baf1addbd5d0d789e3396
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-11 16:46:28 +0000

    using lu as format specifier

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <Python.h>
+#include "Python.h"
 #include <stddef.h>
+#include <time.h>
 
 /**
  * print_python_list_info - Function to print information about python list
@@ -16,12 +17,12 @@ void print_python_list_info(PyObject *p)
 	PyObject *element;
 
 	size = PyList_Size(p);
-	printf("[*] Size of the Python List = %u\n", size);
+	printf("[*] Size of the Python List = %lu\n", size);
 	mem_alloc = ((PyListObject *)(p))->allocated;
-	printf("[*] Allocated = %u\n", mem_alloc);
+	printf("[*] Allocated = %lu\n", mem_alloc);
 	for (i = 0; i < size; i++)
 	{
 		element = PyList_GetItem(p, i);
-		printf("Element %u: %s\n", i, Py_TYPE(element)->tp_name);
+		printf("Element %lu: %s\n", i, Py_TYPE(element)->tp_name);
 	}
 }
