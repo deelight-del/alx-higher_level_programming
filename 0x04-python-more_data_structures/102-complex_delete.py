64322cdca8847dcc73009e311a48c13ea52fd121
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-13 16:56:48 +0000

    task 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    """Function to delete a respective value from a dictionary with
+    respective value. All keys with given value should be deleted.
+
+    Args:
+        a_dictionary: A dictionary to modify
+        value: A given value of different types. Used to delete members of
+        a_dictionary, if it can't be found, nothing happens.
+    """
+    keys_to_delete = []
+    for k, v in a_dictionary.items():
+        if v == value:
+            keys_to_delete.append(k)
+    for k in keys_to_delete:
+        del(a_dictionary[k])
+    return a_dictionary
