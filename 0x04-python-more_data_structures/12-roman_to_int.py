acd7077911c406059d57075350e0ac92abf5f854
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-13 15:34:29 +0000

    pycode function definiton

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    """Function to convert roman_string into integer
+
+    Args:
+        roman_string: The roman string to convert to integer
+
+    Returns:
+        It returns an intger, which is an equivalent of the roman string
+    """
     if not (isinstance(roman_string, str)):
         return 0
     roman = dict(M=1000, C=100, D=500, X=10, L=50, V=5, I=1)
