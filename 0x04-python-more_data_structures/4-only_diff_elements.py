22d636a305910f87ab5773cbec34d15789d1474c
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-13 14:08:11 +0000

    task 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """Function to calculate the symmetric difference between two
+    different sets.
+
+    Args:
+        set_1: First set operand
+        set_2: Second Set operand
+    """
+    return set_1 ^ set_2
