a139fa1f067f084832b2cdd9069f2d5a25bcbb2d
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-13 14:03:43 +0000

    task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """Function to return the set of elements that is common between set1
+    and set2
+
+    Args:
+        set_1: A set object1
+        set_2: A set object2
+    """
+    return set_1.intersection(set_2)
