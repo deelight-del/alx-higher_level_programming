2d8730f83a3506d5d8907a65be4e1e03e70a7fd1
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-13 14:12:07 +0000

    task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """Function to count number of keys of a given dictionary
+
+    Args:
+        a_dictionary: Dictionary to count keys of
+
+    Returns:
+        An integer. The length of the keys
+    """
+    return len(list(a_dictionary))
