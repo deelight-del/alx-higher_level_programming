13547a2c9195e7ee0a0cf4c52b500f3c066f9af4
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-25 19:22:10 +0000

    task 0: refit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -19,7 +19,7 @@ def safe_print_list(my_list=[], x=0):
                 print()
                 if i <= 0:
                     return 0
-                return i
+                return (i + 1)
         print()
         return (i + 1)
     else:
