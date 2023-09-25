859c31f50c012babaf9f7b0bc7e23e3be758bc48
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-25 16:26:50 +0000

    fix code to deal with when x == 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    """Function to safe_print the elements in a list and return the 
-        number of printed elements
+    """Function to safe_print the elements in a list and return the
+    number of printed elements
 
     Args:
         my_list: A given list that is passed into the funciton
@@ -11,6 +11,8 @@ def safe_print_list(my_list=[], x=0):
         An integer that reperesents the number of actual elements printed
     """
 
+    if (x == 0):
+        return(0)
     if isinstance(my_list, list):
         try:
             for i in range(0, x):
@@ -19,7 +21,7 @@ def safe_print_list(my_list=[], x=0):
             if i == 0:
                 return(0)
             print()
-            return (i)
+            return(i)
         else:
             print()
             return(i + 1)
