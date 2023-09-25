a8f8f843036eef97be54a44eaafed00fe74ebe3f
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-25 19:31:03 +0000

    task 4: reload and remove unneccessary

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,20 +4,20 @@ def list_division(my_list1, my_list2, list_length):
     final_list = [0] * list_length
     max_len = max(len(my_list1), len(my_list2))
 
-    for i in range(0, max_len):
+    for i in range(0, list_length):
         try:
             a = my_list1[i]
             b = my_list2[i]
-            final_list[i] = a / b
+            c = a / b
         except TypeError:
             print("wrong type")
-            final_list[i] = 0
+            c = 0
         except ZeroDivisionError:
             print("division by 0")
-            final_list[i] = 0
+            c = 0
         except IndexError:
-            print("Out of range")
-            final_list[i] = 0
+            print("out of range")
+            c = 0
         finally:
-            pass
+            final_list[i] = c
     return final_list
