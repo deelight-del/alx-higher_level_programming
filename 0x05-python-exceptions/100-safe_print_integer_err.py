13977e02922132cacf71e1d76246d9888dadb0d4
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-25 21:06:35 +0000

    task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Function to safe print an integer or error to stderr
+
+    Args:
+        value: The given integer to print.
+
+    Return:
+        True or False. Depending on behaviour of program
+    """
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as e:
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(str(e))
+        sys.stderr.write("\n")
+        return False
