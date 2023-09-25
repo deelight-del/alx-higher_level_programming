3d8c15cc9912320db8018f9e7e2cb2c7a2ae373f
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-25 19:20:46 +0000

    task 0: refactor code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,15 +12,15 @@ def safe_print_list(my_list=[], x=0):
     """
 
     if isinstance(my_list, list):
-        try:
-            for i in range(0, x):
+        for i in range(0, x):
+            try:
                 print(my_list[i], end="")
-            print()
-            return (i + 1)
-        except Exception as e:
-            print()
-            if i <= 0:
-                return 0
-            return i
+            except IndexError as e:
+                print()
+                if i <= 0:
+                    return 0
+                return i
+        print()
+        return (i + 1)
     else:
         return 0
