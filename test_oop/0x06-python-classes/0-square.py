d7e61a5fc4d2327d35d9bf30fb0bdf5c5c0d8988
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-26 18:25:48 +0000

    fix documentation 2

--- test_oop/0x06-python-classes/0-square.py
+++ test_oop/0x06-python-classes/0-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""This file is going to 
+
+"""This file is going to
 contain the class definition of
 Square and its respective attributes
 """
 class Square:
-    """A class that abstracts the data 
+
+    """Square A class that abstracts the data
     square.
+
+
     Takes no attribute for now.
     """
     pass
-
