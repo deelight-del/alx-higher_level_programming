84ab138ff40f953e2eda93bbde48e1c0f469aff9
deelight-del <akingbenidavidoyin@gmail.com>
2023-09-27 17:26:05 +0000

    task 101

--- 0x06-python-classes/test.py
+++ 0x06-python-classes/test.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
-Square = __import__('101-square').Square
 
-my_square = Square(5, (0, 0))
-print(my_square)
+Square = __import__('102-square').Square
 
-print("--")
+s_5 = Square(5)
+s_6 = Square(6)
 
-my_square = Square(5, (4, 5))
-print(my_square)
-
-print("--")
-
-my_square = Square(0, (4, 1))
-print(my_square)
+if s_5 < s_6:
+    print("Square 5 < Square 6")
+if s_5 <= s_6:
+    print("Square 5 <= Square 6")
+if s_5 == s_6:
+    print("Square 5 == Square 6")
+if s_5 != s_6:
+    print("Square 5 != Square 6")
+if s_5 > s_6:
+    print("Square 5 > Square 6")
+if s_5 >= s_6:
+    print("Square 5 >= Square 6")
