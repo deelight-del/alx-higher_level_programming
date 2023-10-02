8fb89d0a9b2942ffb273215ce51615aacd10229f
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-02 11:19:27 +0000

    fix pycodestyle issue

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module contains the
-class definition of the class Rectangle and 
+class definition of the class Rectangle and
 its respective methods, and attributes.
 """
+
+
 class Rectangle:
-    """Class definition of 
+    """Class definition of
     Rectangle, including methoda and attributes
     """
     pass
-
