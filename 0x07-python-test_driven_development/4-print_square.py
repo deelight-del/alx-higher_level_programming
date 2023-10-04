7c2b2541bea714aa09d1e492fcc105888565b104
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-04 16:08:24 +0000

    print blank line for 0.0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -16,14 +16,18 @@ def print_square(size):
         None. But prints output to the screen.
     """
 
+    if size == 0:
+        print()
+        return
+
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print()
+   # if size == 0:
+       # print()
 
-    elif size == 1:
+    if size == 1:
         print("#")
 
     else:
