f6650b9d672d7941851e686dd602badd11a7cbd6
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-04 12:56:58 +0000

    task 2 fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,6 +4,7 @@ for the definition of the matrix dvision of by a number
 div"
 """
 
+
 def matrix_divided(matrix, div):
     """matrix is a list of lists. It will contain
     only numbers. Otherwise, throws error. This function
@@ -14,20 +15,25 @@ def matrix_divided(matrix, div):
         div: Number that is non zero
     """
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)\
+                of integers/floats")
     for i, ls in enumerate(matrix):
         if type(ls) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix\
+                    (list of lists) of integers/floats")
         for val in ls:
             if type(val) is not int and type(val) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a\
+                        matrix (list of lists) of integers/floats")
         if i == 0:
             size = len(ls)
             if size == 0:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix\
+                        (list of lists) of integers/floats")
         else:
             if (size != len(ls)):
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the matrix \
+                        must have the same size")
     if type(div) is not float and type(div) is not int:
         raise TypeError("div must be a number")
     if (div == 0):
