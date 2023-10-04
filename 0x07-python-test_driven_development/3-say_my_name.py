571942a13d3e6fc5b55e2eb751f8fc0f77a9dceb
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-04 14:59:17 +0000

    add documentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,15 @@ given to it.
 
 
 def say_my_name(first_name, last_name=""):
+    """Function to print my name and last name.
+
+    Args:
+        first_name: fist nmae to print
+        last_name: last name of user
+
+    Return:
+        None. But printss to stdout
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
