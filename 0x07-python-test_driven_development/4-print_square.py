dd7eb513f0b4bf56dbe3e42e9dca0071fa355e24
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-04 16:03:27 +0000

    use print() instead of print()

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -21,7 +21,7 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if size == 0:
-        print("")
+        print()
 
     elif size == 1:
         print("#")
