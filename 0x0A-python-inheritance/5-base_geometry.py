b7ec21c1b89b5f3ef6a6002ba6d2654f7925bd06
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-09 16:57:05 +0000

    task 5g

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains the class definition
+of BaseGeometry that contains certain attributes and
+methods for certain functionalities.
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry is a class that inherits directly from
+    object (Python 3.x synatx) and will contain attributes
+    and methods to do stuffs.
+    """
+    pass
