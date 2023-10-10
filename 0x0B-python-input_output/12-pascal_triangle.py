61d64739966d51e74f774577ad8b44d54fd86961
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-10 20:26:38 +0000

    add more documentation

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,23 +5,36 @@ list of lists that reperesents the pascal triangle
 """
 
 
-#def next_triangle(prev_list):
-#    """Function that takes the previous list and outputs the
-#    ncxt list of the pascal triangle
-#    """
-#    next_list = [1]
-#    prev = prev_list[0]
-#    for ele in prev_list[1:]:
-#        result = ele + prev
-#        next_list.append(result)
-#        prev = ele
-#    next_list.append(1)
-#    return next_list
-#
+def next_triangle(prev_list):
+    """Function that takes the previous list and outputs the
+    ncxt list of the pascal triangle
+
+    Args:
+        prev_list: prev_list to generate the next triangle.
+
+    Return:
+        None
+    """
+    next_list = [1]
+    prev = prev_list[0]
+    for ele in prev_list[1:]:
+        result = ele + prev
+        next_list.append(result)
+        prev = ele
+    next_list.append(1)
+    return next_list
+
 
 def pascal_triangle(n):
     """This is the function that returns a pascal triangle
-    according to n that is passed into the function"""
+    according to n that is passed into the function
+
+    Args:
+        prev_list: prev_list to generate the next triangle.
+
+    Return:
+        None
+    """
     if n <= 0:
         return []
     elif n == 1:
@@ -31,6 +44,6 @@ def pascal_triangle(n):
     list_of_lists = [[1], [1, 1]]
     n_iter = n - 2
     for i in range(n_iter):
-        result = 4 #next_triangle(list_of_lists[-1])
+        result = next_triangle(list_of_lists[-1])
         list_of_lists.append(result)
     return list_of_lists
