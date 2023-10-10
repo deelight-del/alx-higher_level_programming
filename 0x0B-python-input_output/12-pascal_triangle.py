e553f7ff71dfe251a528797b10e438e3e82538b3
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-10 20:22:01 +0000

    task 12 - test

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,19 +5,19 @@ list of lists that reperesents the pascal triangle
 """
 
 
-def next_triangle(prev_list):
-    """Function that takes the previous list and outputs the
-    ncxt list of the pascal triangle
-    """
-    next_list = [1]
-    prev = prev_list[0]
-    for ele in prev_list[1:]:
-        result = ele + prev
-        next_list.append(result)
-        prev = ele
-    next_list.append(1)
-    return next_list
-
+#def next_triangle(prev_list):
+#    """Function that takes the previous list and outputs the
+#    ncxt list of the pascal triangle
+#    """
+#    next_list = [1]
+#    prev = prev_list[0]
+#    for ele in prev_list[1:]:
+#        result = ele + prev
+#        next_list.append(result)
+#        prev = ele
+#    next_list.append(1)
+#    return next_list
+#
 
 def pascal_triangle(n):
     """This is the function that returns a pascal triangle
@@ -31,6 +31,6 @@ def pascal_triangle(n):
     list_of_lists = [[1], [1, 1]]
     n_iter = n - 2
     for i in range(n_iter):
-        result = next_triangle(list_of_lists[-1])
+        result = 4 #next_triangle(list_of_lists[-1])
         list_of_lists.append(result)
     return list_of_lists
--- 0x0B-python-input_output/test.py
+++ 0x0B-python-input_output/test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-next_triangle = __import__('12-pascal_triangle').next_triangle
+#next_triangle = __import__('12-pascal_triangle').next_triangle
 pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
-print(next_triangle([1, 1]))
-print(next_triangle([1, 2, 1]))
+#print(next_triangle([1, 1]))
+#print(next_triangle([1, 2, 1]))
 
 print(pascal_triangle(1))
 print(pascal_triangle(2))
