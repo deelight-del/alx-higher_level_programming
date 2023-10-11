133a0e263a05746879ef6349b0c10626db29b2f7
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-11 03:56:01 +0000

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module contains the class definition of Student
+that conatins different attributes and methods.
+"""
+
+
+class Student:
+    """The defintion of the Student class with multiple
+    methods and attributes alike
+    """
+    def __init__(self, first_name, last_name, age):
+        """Defining the init method of the class to first_name,
+        last_name, and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """This reterives the dictionary reperesentation of the class
+        instance
+        """
+        if type(attrs) == list:
+            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Function/method to relaad all the the attributes of the class
+        from a json
+        """
+        self.__dict__ = json
