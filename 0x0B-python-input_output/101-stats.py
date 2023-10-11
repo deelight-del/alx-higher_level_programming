0967b3d9e8041d3befc361036f870406b01e2902
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-11 12:31:13 +0000

    made status code as integers

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,8 +8,8 @@ import sys
 
 
 if __name__ == "__main__":
-    logs = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-            "404": 0, "405": 0, "500": 0}
+    logs = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
+            404: 0, 405: 0, 500: 0}
     file_size = 0
     epoch = 0
 
@@ -18,7 +18,7 @@ if __name__ == "__main__":
             epoch = epoch + 1
             line_list = line.split()
             file_size += int(line_list[-1])
-            logs[line_list[-2]] += 1
+            logs[int(line_list[-2])] += 1
             if epoch % 10 == 0:
                 print("File size: {:d}".format(file_size))
                 for key in logs:
