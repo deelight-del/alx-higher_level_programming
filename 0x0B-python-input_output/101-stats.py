a00e00723bdd563768eebe203d405d11904e671c
deelight-del <akingbenidavidoyin@gmail.com>
2023-10-11 12:25:11 +0000

    task 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,18 +7,24 @@ certain characteristics
 import sys
 
 
-logs = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-        "404": 0, "405": 0, "500": 0}
-file_size = 0
-epoch = 0
+if __name__ == "__main__":
+    logs = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+            "404": 0, "405": 0, "500": 0}
+    file_size = 0
+    epoch = 0
 
-for line in sys.stdin:
-    epoch = epoch + 1
-    line_list = line.split()
-    print(line_list)
-    file_size += int(line_list[-1])
-    logs[line_list[-2]] += 1
-    if epoch % 10:
+    try:
+        for line in sys.stdin:
+            epoch = epoch + 1
+            line_list = line.split()
+            file_size += int(line_list[-1])
+            logs[line_list[-2]] += 1
+            if epoch % 10 == 0:
+                print("File size: {:d}".format(file_size))
+                for key in logs:
+                    if logs[key] > 0:
+                        print("{}: {}".format(key, logs[key]))
+    except (KeyboardInterrupt):
         print("File size: {:d}".format(file_size))
         for key in logs:
             if logs[key] > 0:
--- 0x0B-python-input_output/test.py
+++ 0x0B-python-input_output/test.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+Student = __import__('11-student').Student
 
+def print_dict(d, t=0):
+    keys = list(d.keys())
+    keys.sort()
+    for k in keys:
+        v = d[k]
+        if type(v) is dict:
+            t += 1
+            print_dict(v, t)
+        else:
+            print("{}{} => {} / {}".format("\t" * t, k, v, type(v)))
 
-append_after = __import__('100-append_after').append_after
-
-append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+student = Student("J", "S", 1)
+student.reload_from_json({ 'first_name': "Kevin", 'last_name': "Mc Joe", 'age': 43 })
+j_student = student.to_json()
+print_dict(j_student)
+print_dict(student.__dict__)
