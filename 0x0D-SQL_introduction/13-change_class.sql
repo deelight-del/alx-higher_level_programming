e3c37c48eea5c4464ed415063e95e082205eb855
king <akingbenidavid@gmail.com>
2023-11-14 09:03:11 -0800

    use drop

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,5 +1,3 @@
 -- File that is used to manipulate second_table
 -- The corresponding query for the above
-SELECT * FROM second_table
-WHERE score > 5;
-COMMIT;
+DELETE FROM second_table WHERE score <= 5;
