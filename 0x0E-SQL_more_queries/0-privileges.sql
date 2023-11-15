4e9bc8fc7d741ff067c0b31615ab123dc28f5c33
king <akingbenidavid@gmail.com>
2023-11-15 10:51:43 -0800

    add localhost

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- File that used to show privilleges
 -- code below
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
