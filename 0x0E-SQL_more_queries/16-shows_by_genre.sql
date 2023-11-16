d20ff1c60ba07e3af31212a6bf6ca0ca5599c4f8
deelight-del <akingbenidavidoyin@gmail.com>
2023-11-16 09:00:41 +0000

    task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Manipulate db and table
+-- CODE below.
+SELECT tv_shows.title, tv_genres.name FROM
+tv_shows LEFT OUTER JOIN 
+(tv_show_genres INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id)
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_genres.name ASC;
