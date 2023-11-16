00fa004c7309c290babe9c3cc9a042b5e3e92b84
deelight-del <akingbenidavidoyin@gmail.com>
2023-11-16 07:41:00 +0000

    task 13

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,9 @@
 -- CODE below.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 --USE hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id UNIQUE AUTO_INCREMENT INT PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	CONSTRAINT fk_cities FOREIGN KEY(state_id) REFERENCES states(id)
+	CONSTRAINT fk_cities FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 	) ENGINE=InnoDB;
