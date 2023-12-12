5ec8c1431c4648115f6f95bdeefc10e3f9373a7f
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-12 01:46:12 +0000

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const language = ['c', 'Python', 'JavaScript'];
+const feature = ['fun', 'cool', 'amazing'];
+for (let i = 0; i <= 2; i++) {
+  console.log(`${language[i]} is ${feature[i]}`);
+}
