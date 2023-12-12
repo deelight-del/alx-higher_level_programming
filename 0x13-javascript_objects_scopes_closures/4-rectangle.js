7d67aa9c94b0acf863990201c73e5aea0579c979
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-12 15:59:12 +0000

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const string = 'X'.repeat(this.width);
+      console.log(string);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
