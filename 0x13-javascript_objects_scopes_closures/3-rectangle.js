8037895511563be227c4b6d71c8227698da84ba3
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-12 15:53:21 +0000

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const string = 'X'.repeat(this.width);
+      console.log(string);
+    }
+  }
+}
+
+module.exports = Rectangle;
