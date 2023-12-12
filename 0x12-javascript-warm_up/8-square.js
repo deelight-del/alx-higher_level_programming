434454b100c923e93b62f49d8e8c10525d707793
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-12 02:18:34 +0000

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const argvTwo = process.argv[2];
+
+function isNumber (number) {
+  if (isNaN(number)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+if (isNumber(argvTwo)) {
+  const loopTimes = Number(argvTwo);
+  for (let i = 0; i < loopTimes; i++) {
+    let repeatedStr = 'X'.repeat(loopTimes);
+    console.log(repeatedStr);
+  }
+} else {
+  console.log('Missing size');
+}
