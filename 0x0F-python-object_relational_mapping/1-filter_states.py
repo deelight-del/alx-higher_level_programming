0021c82322c0d78cf2ce89d43117f15bf7477c38
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-15 15:08:39 +0000

    task 1 refix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     cur = conn.cursor()
     cur.execute(
         "SELECT * FROM states WHERE name LIKE\
-        'N%' ORDER BY id ASC"
+        'N%' ORDER BY states.id ASC"
         )
     rows = cur.fetchall()
     for row in rows:
