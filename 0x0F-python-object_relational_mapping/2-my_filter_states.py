c7800e1598a3eae8c1a0532ebe3181deb82ccb38
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-15 13:56:30 +0000

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,14 +8,18 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
+    search_name = sys.argv[4]
     conn = MySQLdb.connect(
-        host="localhost", port=3306, db=db_name,
-        user=username, passwd=password, charset="utf8"
+        host="localhost", port=3306, user=username,
+        passwd=password, db=db_name, charset="utf8"
         )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}%' ORDER BY id ASC".format("N"))
-    rows = cur.fetchall()
-    for row in rows:
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}'\
+        ORDER BY id ASC".format(search_name)
+        )
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
     cur.close()
     conn.close()
