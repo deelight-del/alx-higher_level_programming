1186f6e8ee7b32d3e66aff9804f9c86c4911770c
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-15 19:17:10 +0000

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This module contains the ORM for objects
+that contain only a"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import asc, create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter(
+        State.name.contains('a')).order_by(asc(State.id))
+    for obj in result:
+        print(str(obj.id) + ":", obj.name)
