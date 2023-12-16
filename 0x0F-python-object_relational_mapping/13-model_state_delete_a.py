d3a53c08a05f5b68ab335b980d4d7352ef8446de
deelight-del <akingbenidavidoyin@gmail.com>
2023-12-16 08:02:07 +0000

    fix issue in task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.query(State).filter(State.name.contains('a')).drop()
+    session.query(State).filter(State.name.contains('a')).delete()
     session.commit()
