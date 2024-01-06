ff31a77a4374b77ba9768a75bc9762f0ae3b84b3
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 02:34:46 +0000

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains the technical interview to get
+the peak of an sorted list"""
+
+
+def find_peak(list_of_integers):
+    """Function definition of the peak"""
+    return (max(list_of_integers))
