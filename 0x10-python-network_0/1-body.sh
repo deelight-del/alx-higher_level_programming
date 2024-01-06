4c14197e44936793d93d851f8412115c76b0065b
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 01:16:51 +0000

    Fix task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # curl to send in a get request and print the body of respons
 response=$(curl -G -s -w "%{http_code}" -o /dev/null "$1")
-#echo "$response"
 if [ "$response" = "200" ]; then
 	curl -G -s "$1";
 fi
