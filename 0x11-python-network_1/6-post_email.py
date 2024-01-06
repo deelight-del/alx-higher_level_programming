d400b90405bee33e9936f5e85890206a6f2c9369
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 17:56:08 +0000

    let's see for task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,3 +5,9 @@
 if __name__ == "__main__":
     import requests
     import sys
+
+    response = requests.post(
+                sys.argv[1],
+                data = {'email': sys.argv[2]}
+            )
+    print(response.text)
