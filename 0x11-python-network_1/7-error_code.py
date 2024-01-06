8e88ef3147821d0786516844a8287359217c4e88
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 14:19:08 +0000

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A Python script that takes in a URL,
+sends a request to the URL and displays the
+body of the response.."""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        print(r.text)
+    except requests.exceptions.HTTPError as e:
+        print("Error code:", e.status_code)
