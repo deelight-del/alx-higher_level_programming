d639ac59b3c5ddb9dede291b358804b21f35a3bc
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 15:58:18 +0000

    xxx

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""a Python script that fetches
+
+"""A Python script that fetches
 from the url passed to it, and POST a data sent to it"""
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    req = requests.post(sys.argv[1], data={'email': sys.argv[2])
-    print(req.text)
+    req = requests.post(sys.argv[1], data={'email': sys.argv[2]})
