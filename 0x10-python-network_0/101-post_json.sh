87f0796ed47111991e857bbc1126424f7cdd01ba
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 03:12:10 +0000

    Task 8, no output

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl to send in a get request and print the body of respons
-curl -s -X POST --json @"$2" "$1"
+curl -s -H "content-Type: apllication/json" --data-binary @"$2" -X POST "$1";
