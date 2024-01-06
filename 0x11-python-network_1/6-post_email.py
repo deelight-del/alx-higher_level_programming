00b1f6e04c9ab5e3f85cebdf65366bc6a676552b
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 17:53:39 +0000

    task 6, rewrite

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+"""The use of requests.post"""
 
-"""A Python script that fetches
-from the url passed to it, and POST a data sent to it"""
-
-import requests
-import sys
 
 if __name__ == "__main__":
-    req = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    import requests
+    import sys
