2f6497d34731ada62d48ddd15660e64207e56722
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 02:20:01 +0000

    using the data format to POST data

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl to send in a get request and print the body of respons
-curl -s -X POST -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1";
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1";
