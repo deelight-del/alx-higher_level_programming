d6d60e7b4e4edd5548c560c74c5fda4755aa3744
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 17:28:57 +0000

    Task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    headers = {
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+            }
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    response = requests.get(url, headers)
+    try:
+        json_data = response.json()
+        for eachCommit in json_data[0:10]:
+            print(
+                    eachCommit.get("commit").get("tree").get("sha"), ": ",
+                    eachCommit.get("commit").get("committer").get("name")
+                    )
+    except (
+            requests.exceptions.JSONDecodeError,
+            requests.exceptions.HTTPError
+            ):
+        pass
