1af55e3b4976185c399de831d1efdc2fa5ecbc7e
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 02:15:22 +0000

    fix task5: using post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl to send in a get request and print the body of respons
-curl -s -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1";
+curl -s -X POST -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1";
