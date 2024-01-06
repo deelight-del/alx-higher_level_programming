766ff5be083fe90f3c94d48ae3ea279b707d7d6c
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 17:57:06 +0000

    fix pycode issue

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
 
     response = requests.post(
                 sys.argv[1],
-                data = {'email': sys.argv[2]}
+                data={'email': sys.argv[2]}
             )
     print(response.text)
