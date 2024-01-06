ba5626e8e762ae371692ed2d4f55f9ddb644199a
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-06 15:17:21 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""a Python script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter."""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = "http://0.0.0.0:5000/search_user"
+    payload = dict()
+    try:
+        payload['q'] = sys.argv[1]
+    except IndexError:
+        payload['q'] = ""
+    response = requests.post(url, data=payload)
+    try:
+        response.raise_for_status()
+        json_format_data = response.json()
+        if not json_format_data:
+            print("No result")
+        else:
+            result = (
+                    "[" + str(json_format_data['id']) + "] " +
+                    json_format_data['name']
+                    )
+            print(result)
+    except (
+            requests.exceptions.HTTPError,
+            Exception
+            ):
+        if int(respons.status_code) == 204:
+            print("No result")
+        else:
+            print("Not a valid json")
