3ad5338469f706ff16d7e026390d067fb2d36186
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-30 13:00:37 +0000

    Task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const process = require('process');
+const idURL = 'https://swapi-api.alx-tools.com/api/people/18/';
+const filmURL = process.argv[2];
+let count = 0;
+request(filmURL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = (JSON.parse(body)).results;
+    for (let i = 0; i < films.length; i++) {
+      if (films[i].characters.includes(idURL)) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
