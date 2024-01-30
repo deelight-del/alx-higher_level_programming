af51f57844c2fd0c681c41d1f1044fa3bc4a143b
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-30 11:48:31 +0000

    Task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+const filePath = process.argv[2];
+fs.readFile(filePath, function(err, data) {
+  try {
+    console.log(data.toString());
+  } catch (error) {
+    console.log(err);
+  }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,4 @@
+# JAVASCRIPT WEB SCRAPING
+
+This contains the directory where we use
+JS to read files and do other JSON parsing.
